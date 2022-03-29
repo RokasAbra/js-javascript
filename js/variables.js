4a413266d34e9011b489011ebe8ef02e14f4fb5d
RokasAbra <rokasabramavicius@gmail.com>
2022-03-29 13:11:42 +0300

    Variables

---
+++ js/variables.js
@@ -0,0 +1,64 @@
+/*
+
+
+KINTAMIEJI
+
+Iniciavimo budas:
+const - default iniciavimo budas
+let - antrinis budas inicijuoti, kai zinome, jog reiksme tures keistis
+var -pats seniausias ir geriau niekada nenaudoti, jei rupi saugumas
+
+
+Duomenu tipas: 
+- number, 
+- string (tekstas),  
+- bolean (logine reiksme: true, false),
+- array (masyvas, sarasas, matrica*)
+
+*/
+// vienos eilutes komentaras ( // ) (/* */ keliu eiluciu komentaras)
+
+const myLuckyNumberIs = 7;
+
+console.log('Lucky number',myLuckyNumberIs);
+
+let pinigine = 0;
+console.log('pinigine:',pinigine);
+
+ pinigine = 5;
+console.log('pinigine:',pinigine);
+pinigine = 15;
+console.log('pinigine:',pinigine);
+
+
+const vardas = 'Rokas';
+const pasisveikinimas = ' Laba diena, ' + vardas + '.';
+console.log(vardas);
+
+console.log(' Laba diena, ' + vardas + '.' );
+
+console.log(pasisveikinimas);
+
+const arVedes = true;
+const arTuriVaiku = false;
+const arMeluojaDelVaiku = true;
+
+console.log('ar vedes?',arVedes);
+console.log('ar turi vaiku?',arTuriVaiku);
+console.log('ar meluoja?',arMeluojaDelVaiku);
+
+console.log(2+2*2);
+console.log('2+2*2');
+console.log(true, false);
+console.log('true', 'false');
+
+const pazymiai = [10, 2, 8, 4, 6];
+console.log('Pazymiai', pazymiai);
+
+const studentai = ['Petras', 'Jonas', 'Rokas', 'Ona'];
+console.log(studentai);
+
+const booleanList = [true, false, true, false, true, false]
+console.log(booleanList);
+
+
