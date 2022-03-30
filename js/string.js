24ad7541e8d68c0924352a2d3bc363b5a765b71f
RokasAbra <rokasabramavicius@gmail.com>
2022-03-30 11:37:36 +0300

    Strings

---
+++ js/string.js
@@ -0,0 +1,39 @@
+/*
+STRING
+
+kabutes: 
+- Viengubos(' ')
+- Dvigubos (" ")
+-----------------------
+\ - back slahsas pries kabutes ( \') ( \") blokuoja 
+'cia yra vienguba(\') ir dvyguba (") kabutes.';
+'cia yra vienguba(\') ir dvyguba (\") kabutes';
+*/
+
+const name = 'Rokas';
+console.log(name);
+
+const lastname = "Abramavicius";
+console.log(lastname);
+
+const viengubaKabute = " Viengubos(' ') ";
+console.log(viengubaKabute);
+
+const dvygubaKabute = ' Dvigubos (" ") ';
+console.log(dvygubaKabute);
+
+const viengubakabute12 = 'cia yra vienguba(\') ir dvyguba (") kabutes.';
+console.log(viengubakabute12);
+
+const dvygubakabute12 = "cia yra vienguba(') ir dvyguba (\") kabutes.";
+console.log(dvygubakabute12);
+
+const kabutes12 = 'cia yra vienguba(\') ir dvyguba (\") kabutes';
+console.log(kabutes12);
+console.log('---------------------');
+const clientName = 'Rokas';
+const clientAge = 28;
+// Client Rokas is 28 years old
+const clientSentence = 'Client ' + clientName + ' is ' + clientAge + ' years old.';
+console.log(clientSentence);
+
