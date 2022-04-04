342e4f8b6ee4f167d4d0cf1d7584b78683bd46e0
RokasAbra <rokasabramavicius@gmail.com>
2022-04-04 12:22:53 +0300

    Reverse string

---
+++ js/10-reverse-string.js
@@ -0,0 +1,45 @@
+function reverseString(text) {
+    
+    let reverse = '';
+    for (let i = 0; i < text.length; i++) {
+        const position = text.length - 1 - i;
+        reverse += text[position];
+        
+    }
+    return reverse;
+}
+console.log(reverseString('abcdef'), '-->', 'fedcba');
+console.log(reverseString('Labas'), '-->', 'sabaL');
+console.log('--------------------');
+
+
+
+function reverseString2(text) {
+   let reverse = '';
+
+   for (let i = 0; i < text.length; i++) {
+       reverse = text[i] + reverse;
+   }
+    return reverse;
+
+} 
+console.log(reverseString2('abcdef'), '-->', 'fedcba');
+console.log(reverseString2('Labas'), '-->', 'sabaL');
+console.log(reverseString2('xy'), '-->', 'yx');
+
+console.log('---------------');
+function reverseString3(text) {
+   let reverse = '';
+   
+   for (let i=text.length-1; i>=0; i--){
+       reverse += text[i]
+   }
+   
+    return  reverse;
+}
+     
+
+console.log(reverseString3('abcdef'), '-->', 'fedcba');
+console.log(reverseString3('Labas'), '-->', 'sabaL');
+console.log(reverseString3('xy'), '-->', 'yx');
+
